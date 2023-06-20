c0dd6ee9d60308a7bd77d5f7c311d8ec35410a80
IlyaZaporojec <ilya.zaporojec@gmail.com>
2023-06-21 00:16:36 +0300

    add slaider

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,5 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 import "./modules/video.js";
 import "./modules/back-to-top-btn.js";
+import "./modules/slick.js";
+import "./modules/slaider.js";
