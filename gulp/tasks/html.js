b0edecf3e5ceb0da24d29731b7be811abec35988
Valentynmrzv <bmvalentin27@gmail.com>
2023-06-20 22:44:27 +0300

    GULP

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,7 +1,10 @@
+import gulp from "gulp";
+import fileinclude from "gulp-file-include";
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
 import versionNumber from "gulp-version-number";
 
 export const html = () => {
-    return (
+  return (
     app.gulp
       .src(app.path.src.html)
       .pipe(
@@ -11,7 +14,8 @@ export const html = () => {
             message: "Error:<%= error.message %>",
           })
         )
-            .pipe(fileinclude())
+      )
+      .pipe(fileinclude())
       .pipe(app.plugins.replace(/@img\//g, "img/"))
       .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
       // .pipe(
