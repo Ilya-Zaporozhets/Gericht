cbb5565cc01613948d10b270aef339c2d3dd7506
IlyaZaporojec <ilya.zaporojec@gmail.com>
2023-06-20 19:12:40 +0300

    add video-player

---
+++ src/js/modules/back-to-top-btn.js
@@ -0,0 +1,8 @@
+import { addBackToTop } from 'vanilla-back-to-top';
+
+addBackToTop({
+  diameter: 35,
+  backgroundColor: var(--bcg-color),
+  textColor: var(--main-color),
+  scrollDuration: 500,
+});
